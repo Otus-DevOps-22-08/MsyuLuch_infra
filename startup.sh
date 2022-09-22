1e2be0a5ba002ce2f5e74bb9ea93fd040e8b151d
MsyuLuch <mashkina@gmail.com>
2022-09-22 15:16:22 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 sudo apt update
 sudo apt-get install -y mongodb-org
 
-sudo systemctl start mongodb && sudo systemctl enable mongodb && sudo systemctl status mongodb
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
 
 git clone -b monolith https://github.com/express42/reddit.git
 
