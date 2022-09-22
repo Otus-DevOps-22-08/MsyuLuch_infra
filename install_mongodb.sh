8340860e05d49c9f1b3a284e4d15263964104236
MsyuLuch <mashkina@gmail.com>
2022-09-22 16:04:38 +0900

    Update startup.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
 sudo apt update
-sudo apt-get install -y gnupg
 sudo apt-get install -y mongodb-org
 
 sudo systemctl start mongod
--- startup.sh
+++ startup.sh
@@ -3,11 +3,10 @@
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
 sudo apt update
-sudo apt-get install -y gnupg
 sudo apt-get install -y mongodb-org
 
 sudo systemctl start mongod
